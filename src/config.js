95fb91fc474f31cea8e716ad7daf2cf4accc7e58
rogtang <rogtang@gmail.com>
2020-07-05 21:30:40 -0700

    initial commit

---
+++ src/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    PORT: process.env.PORT || 8000,
+    NODE_ENV: process.env.NODE_ENV || 'development',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/bookmarks',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/bookmarks-test',
+    JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
+  }
